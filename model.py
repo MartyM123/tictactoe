a9dd253ff9fd93ae91d371b809b1a2749ee39b12
MartyM123 <86890275+MartyM123@users.noreply.github.com>
2024-04-17 21:52:37 +0200

    finished model without learning

--- model.py
+++ model.py
@@ -4,8 +4,13 @@ class layer():
     def __init__(self, n):
         self.n = n
 
-class input_layer(layer):
-    def compute(input : np.ndarray) -> np.ndarray:
+    def __str__(self) -> str:
+        return f'layer with {self.n} nodes'
+
+    def compile(self, *args, **kwkwargs):
+        pass
+
+    def compute(self, input : np.ndarray) -> np.ndarray:
         return input
 
 class dense(layer):
@@ -14,14 +19,37 @@ class dense(layer):
         n_before the size of input or the size of output of previous layer
         function sets weights and biases for this layer''' 
         self.n_before = n_before
-        self.weights = (np.ones(self.n*n_before)/4).reshape((n_before, self.n))
+        self.weights = (np.ones(self.n*n_before)/4).reshape((self.n, n_before))
         self.biases = np.ones(self.n)/10
     
     def compute(self, input) -> np.ndarray:
         computed = np.sum(input*self.weights, axis=1)
-        return np.where(computed>self.biases, computed, 0) #bias filter
-    
+        return computed
 
-Dense=dense(9)
-Dense.compile(9)
-print(Dense.compute(np.array([1,0,0,-1,-1,0,1,1,0])))
+class model:
+    def __init__(self, layers=[]):
+        self.layers=layers
+    
+    def __str__(self) -> str:
+        return str([layer.__str__() for layer in self.layers])
+
+    def compute(self, input : np.ndarray) -> np.ndarray:
+        output = input
+        for layer in self.layers:
+            output = layer.compute(output)
+        return output
+    
+    def compile(self):
+        for i in range(len(self.layers)-1):
+            self.layers[i+1].compile(n_before=self.layers[i].n)
+
+    
+Model=model()
+Model.layers=[layer(3), dense(4), dense(3)]
+
+Model.compile()
+
+Dense = dense(4)
+Dense.compile(3)
+
+print(Model.compute(np.array([1,2,3])))
