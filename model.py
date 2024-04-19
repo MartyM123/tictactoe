89e68b7107c35358bea2cd09a93d21482113e47e
MartyM123 <86890275+MartyM123@users.noreply.github.com>
2024-04-19 14:04:35 +0200

    mutate func

--- model.py
+++ model.py
@@ -6,6 +6,18 @@ import math
 def sigmoid(x):
   return 1 / (1 + np.exp(-x))
 
+def plain(x):
+    return x
+
+def add_random_n_places(a, n):
+    out = a.astype(float)
+
+    # Generate unique flattened indices along the size of a
+    idx = np.random.choice(a.size, n, replace=False)
+    # Assign into those places ramdom numbers in [-1,1)
+    out.flat[idx] += np.random.uniform(low=-1, high=1, size=n)
+    return out
+
 class layer():
     def __init__(self, n):
         self.n = n
@@ -18,15 +30,18 @@ class layer():
 
     def compute(self, input : np.ndarray) -> np.ndarray:
         return input
+    
+    def mutate(self):
+        pass
 
 class dense(layer):
-    def compile(self, n_before : int, activation_func=sigmoid):
+    def compile(self, n_before : int, activation_func=plain):
         '''
         n_before the size of input or the size of output of previous layer
         function sets weights and biases for this layer''' 
         self.n_before = n_before
-        self.weights = (np.ones(self.n*n_before)/4).reshape((self.n, n_before))
-        self.biases = np.ones(self.n)/10
+        self.weights = np.zeros((self.n, n_before))
+        self.biases = np.zeros(self.n)
         self.activation_func = activation_func
     
     def compute(self, input) -> np.ndarray:
@@ -35,6 +50,11 @@ class dense(layer):
     
     def to_json(self):
         return json.dumps(self)
+    
+    def mutate(self):
+        '''mutation of weights'''
+        self.weights = add_random_n_places(self.weights, 5)
+
 
 class model:
     def __init__(self, layers=[]):
@@ -56,17 +76,20 @@ class model:
     def save(self, name='save'):
         with open(str(name)+'.pkl', 'wb') as save_file:
             pickle.dump(self, save_file)
+    
+    def mutate(self):
+        for layer in self.layers:
+            layer.mutate()
 
 def load_model(name='save'):
     with open(str(name)+'.pkl', 'rb') as load_file:
         return pickle.load(load_file)
 
 Model=model()
-Model.layers=[layer(3), dense(4)]
+Model.layers=[layer(9), dense(9)]
 
 Model.compile()
 
-print(Model)
-print(Model.compute(np.array([1,2,3])))
-
-Model.save()
+print(Model.compute(np.array([1,0,1,0,-1,-1,-1,0,1])))
+Model.mutate()
+print(Model.compute(np.array([1,0,1,0,-1,-1,-1,0,1])))
